873f94c9e5f008428fdc704ada0a0ca37fc93ee2
MAHDI <hassanim430@gmail.com>
2021-03-21 10:51:33 +0430

    initial app

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Home from './Screens/Home';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator} from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Tab.Navigator
+          tabBarOptions = {{
+            activeTintColor: '#841548'
+          }}
+        >
+        <Tab.Screens 
+        name="Home"
+        component={HomeStack}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons 
+              name="home"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        />
+        </Tab.Navigator>      
+    </NavigationContainer>
+  );
+}
+
+function HomeStack(){
+  return (
+    <Stack.Navigator 
+      screenOptions={{
+        headerStyle:{backgroundColor: '#841584'},
+        headerTintColor: '#fff',
+        headerTitleStyle: {fontWeight: 'bold'}
+      }}
+    >
+
+      <Stack.Screen 
+        name="Home"
+        component={Home}
+        options = {{
+            title: "Home Page"
+        }}
+      />
+
+    </Stack.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
---
+++ Components/FoodItems.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+
+export default function FoodItems({name, image, price}) {
+
+  return (
+    <View style={styles.container}>
+     	<Image
+     		style={styles.images} 
+     		source={{ uri: image }} />
+
+     	<View style={{ alignItems:'center' }}>
+     		<Text style={{ fontWeight:'bold' }}>{name}</Text>	
+     		<Text>{price}</Text>
+     	</View>	
+     		
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1
+	},
+	images:{
+		width:100,
+		height:100,
+		borderTopLeftRadius:15,
+		borderBottomRightRadius:15
+	}
+});
